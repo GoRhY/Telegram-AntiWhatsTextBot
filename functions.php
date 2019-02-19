da3cb85f4ac161543dcfd35b4959316c64f92dca
GoRhY <github@inakilopez.com>
2019-02-19 12:32:50 +0100

    Files upload

---
+++ functions.php
@@ -0,0 +1,50 @@
+<?php
+$included_files = get_included_files();
+if (count($included_files)==1){
+	header('Content-Type: text/html; charset=UTF-8');
+}
+date_default_timezone_set('Europe/Madrid');
+
+function imprimir($array){
+	echo "<pre>";
+	print_r($array);
+	echo "</pre>";
+	var_dump($array);
+}
+
+function send($url){
+	echo $url;
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+	$output = curl_exec($ch);
+	curl_close($ch);
+	return $output;
+}
+
+function sendMessage($text,$chat_id,$options=array("")){
+	$defaults = ['parse_mode' => 'html'];
+	$params = array_merge($defaults, $options);
+	$url = 'https://api.telegram.org/bot' . BOT_ID. '/sendMessage?text='.urlencode($text).'&chat_id='.$chat_id.'&'.http_build_query($params);
+	return send($url);
+}
+
+function deleteMessage($chat_id,$message_id,$options=array("")){
+	$defaults = ['parse_mode' => 'html'];
+	$params = array_merge($defaults, $options);
+	$url = 'https://api.telegram.org/bot' . BOT_ID. '/deleteMessage?&chat_id='.$chat_id.'&message_id='.$message_id.'&'.http_build_query($params);
+	return send($url);
+}
+
+function checkMessage($text,$chat_id,$message_id){
+	if ((substr($text,0,8)=="Video de")||(substr($text,0,7)=="Foto de")||(substr($text,0,10)=="Photo from")||(substr($text,0,10)=="Video from")||(substr($text,0,12)=="Screenshot (")){
+		deleteMessage($chat_id,$message_id);
+	}
+}
+
+function leaveChat($chat_id){
+	$url = 'https://api.telegram.org/bot' . BOT_ID . '/leaveChat?chat_id='.$chat_id;
+	return send($url);
+}
+?>
---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+date_default_timezone_set('Europe/Madrid');
+define("BOT_ID", "");
+define("ADMIN_ID", "");
+include("functions.php");
+$result = file_get_contents("php://input");
+if ($result){
+	$update = json_decode($result, true);
+	if (!empty($update)){
+		if ($update["message"]["chat"]["type"]=="group"){ //GROUP
+			$group_id = $update["message"]["chat"]["id"];
+			$message_id = $update["message"]["message_id"];
+			$text = $update["message"]["text"];
+			checkMessage($text,$group_id,$message_id);
+		}else if ($update["message"]["chat"]["type"]=="supergroup"){ //SUPERGROUP/CHANNEL
+			$group_id = $update["message"]["chat"]["id"];
+			$message_id = $update["message"]["message_id"];
+			$text = $update["message"]["text"];
+			checkMessage($text,$group_id,$message_id);
+		}else if ($update["message"]["chat"]["type"]=="private"){ //CHAT
+			$now = strtotime("now");
+			$chat_id = $update["message"]["chat"]["id"];
+			$message_id = $update["message"]["message_id"];
+			$text = $update["message"]["text"];
+			sendMessage("Este bot sirve para borrar los mensajes (EN GRUPOS, requiere permisos de borrado de texto) que se generan automáticamente al compartir algún vídeo o foto desde Whatsapp",$chat_id);
+		}else if ($update["channel_post"]){ //CHANNEL
+				$channel_id = $update["channel_post"]["chat"]["id"];
+				leaveChat($channel_id);
+		}
+	}
+}
+?>
